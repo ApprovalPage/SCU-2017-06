e7b89ff1116d33aff146f48ecf98a85655a2f50c
ApprovalPage <curtisjackson23@gmail.com>
2017-06-19 14:47:17 -0700

    Add files via upload

--- Wheels/160x600/SCU201706_Wheels_160x600_AdR.js
+++ Wheels/160x600/SCU201706_Wheels_160x600_AdR.js
@@ -10,8 +10,8 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/HTML5rastershad.png?1497906921569", id:"HTML5rastershad"},
-		{src:"images/Wheels160x600.jpg?1497906921569", id:"Wheels160x600"}
+		{src:"images/HTML5rastershad.png?1497908683089", id:"HTML5rastershad"},
+		{src:"images/Wheels160x600.jpg?1497908683089", id:"Wheels160x600"}
 	]
 };
 
@@ -587,7 +587,7 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 	// com
 	this.instance = new lib.comhoriz();
 	this.instance.parent = this;
-	this.instance.setTransform(80.1,585.7,0.8,0.8,0,0,0,0.1,0.9);
+	this.instance.setTransform(80,585.8,0.68,0.68,0,0,0,0.1,1);
 	this.instance._off = true;
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(163).to({_off:false},0).to({_off:true},1).wait(1));
@@ -595,7 +595,7 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 	// ncua
 	this.instance_1 = new lib.ncuahorizai();
 	this.instance_1.parent = this;
-	this.instance_1.setTransform(80,572.8,1.2,1.2,0,0,0,0,3.6);
+	this.instance_1.setTransform(80,572.9,1.13,1.13,0,0,0,0.1,3.7);
 	this.instance_1._off = true;
 
 	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(163).to({_off:false},0).to({_off:true},1).wait(1));
